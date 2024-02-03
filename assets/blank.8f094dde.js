96cad4219044132af400d337dcc54aaafc20f232
Rabbi-x1 <rabbialrabbi@gmail.com>
2024-02-03 20:39:59 +0600

    Sells Order : Add labour and collection

--- assets/blank.8f094dde.js
+++
@@ -1,1 +0,0 @@
-import{d as a,h as e,V as n,r as o}from"./index.8ca49e5c.js";const s=a({setup(){const t=o("router-view");return()=>e(n,{class:"layout-wrapper layout-blank"},{default:()=>e(t)})}});export{s as default};
