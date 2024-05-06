f00a7ebd384f5c09ce8c620d53930edd9b18b879
Rabbi-x1 <rabbialrabbi@gmail.com>
2024-05-06 08:26:42 +0600

    Auth : made auth

---
+++ assets/users.20791e95.js
@@ -0,0 +1,1 @@
+import{bg as e,b2 as t,b3 as a}from"./index.f5f13f42.js";const i=e("user",{state:()=>({users:[],editModalStatus:!1}),getters:{},actions:{async getUserList(r){let s=await t.get(a.users);this.users=s.data.data}}});export{i as u};
