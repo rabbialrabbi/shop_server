217fc77cefff69a8e7a823b207be0a455c09b81f
Rabbi-x1 <rabbialrabbi@gmail.com>
2024-05-13 08:28:48 +0600

    Buy Order : Remove option

---
+++ assets/tab.ed4365be.js
@@ -0,0 +1,1 @@
+import{bl as a}from"./index.24ce2781.js";const d={randomNumber:()=>Math.floor(Math.random()*1e3+1)},r=a("tab",{state:()=>({tabs:[],selectedTab:""}),actions:{addTab(t){let s=t.id;this.tabs.find(e=>e.id==s)||null||this.tabs.push(t),this.selectedTab=t.id}}});export{d as h,r as u};
--- assets/users.bf130c3e.js
+++
@@ -1,1 +0,0 @@
-import{bg as e,b2 as t,b3 as a}from"./index.71bb308e.js";const i=e("user",{state:()=>({users:[],editModalStatus:!1}),getters:{},actions:{async getUserList(r){let s=await t.get(a.users);this.users=s.data.data}}});export{i as u};
